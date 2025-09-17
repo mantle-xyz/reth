e6e1d6b5787fe2b1dd6e7383dccc715ac49a3ee3
ivan <314130948@qq.com>
2025-09-17 15:42:08 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-make build-op
+make build-op 
+make install-op
 cp target/release/op-reth /Users/sh001/Documents/codes/rde-v3/src/op-geth/build/bin/op-reth_dump
 ls -l /Users/sh001/Documents/codes/rde-v3/src/op-geth/build/bin/op-reth_dump
